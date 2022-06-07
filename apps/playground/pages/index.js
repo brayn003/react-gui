860056d8466431e274451f0f5a56513a6bae6958
Rudraprasad Das <brayn003@gmail.com>
2022-06-07 20:24:14 +0530

    adding next

---
+++ apps/playground/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+function Home() {
+  return (
+    <div>
+      <Head>React GUI</Head>
+      <h1>Welcome to React GUI</h1>
+    </div>
+  )
+}
+
+export default Home;
